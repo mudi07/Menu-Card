f52f35f68c6e805383faa769d719025ad1dd2456
mudi07 <113351063+mudi07@users.noreply.github.com>
2022-10-11 11:18:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Resturant from "./components/basics/resturant";
+//import UseState from "./components/hooks/usestate";
+//import UseEffect from "./components/hooks/useEffect";
+const App = () => {
+  return <Resturant />;
+};
+
+export default App;
